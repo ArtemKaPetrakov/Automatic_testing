860a7af27af8b4372be10001f23219c936d12701
Artem Petrakov <artem.petrakov@climb-lab.ru>
2024-03-02 20:49:18 +0300

    switch to assert

--- tests/test_capitalize.py
+++ tests/test_capitalize.py
@@ -1,9 +1,15 @@
 from capitalize import capitalize
 
-if capitalize('hello') != 'Hello':
-    raise Exception('Функция работает неверно!')
+# if capitalize('hello') != 'Hello':
+#     raise Exception('Функция работает неверно!')
 
-if capitalize('') != '':
-    raise Exception('Функция работает неверно!')
+# if capitalize('') != '':
+#     raise Exception('Функция работает неверно!')
+
+assert capitalize('') == ''
+assert capitalize('hello') == 'Hello'
 
 print('Все тесты пройдены!')
+
+
+# PYTHONPATH=package_name python tests/test_capitalize.py запуск кода
